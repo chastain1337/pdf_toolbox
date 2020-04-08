9db5b7230739fd6ffa9f39f61a7db0bf32011d91
James Chastain <jameschastain1337@gmail.com>
2020-04-08 15:48:52 -0400

    PDFNode.js

---
+++ src/PDFNode.js
@@ -0,0 +1,9 @@
+//import data from "N:\\DEV\\James\\Stock Posting Scan Check Remote Process for accounting.pdf";
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import React from "react"
+import data from 
+
+export default class PDFNode extends React.Component {
+
+}
