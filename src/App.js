95b1b62ad313d3058c4fac55d660eed6331a437a
James Chastain <jameschastain1337@gmail.com>
2020-04-08 10:36:58 -0400

    added preferences file and corrected a key issue

--- src/App.js
+++ src/App.js
@@ -1,58 +1,106 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import FileExplorer from "./FileExplorer";
+const prefs = require("./preferences.json");
 
 //const electron = window.require("electron");
 
 export default class App extends React.Component {
   addExplorer = () => {
-    this.setState( prevState => {
-      const _explorers = [...prevState.explorers, <FileExplorer removePDFFromViewPort={this.removePDFFromViewPort} addPDFToViewPort={this.addPDFToViewPort} removeExplorer={this.removeExplorer} key={`explorer-${Date.now()}`} id={Date.now()} addExplorer={this.addExplorer}/>]
-      return {explorers: _explorers}
-  });
-}
+    this.setState((prevState) => {
+      const _explorers = [
+        ...prevState.explorers,
+        <FileExplorer
+          initialPath=""
+          removePDFFromViewPort={this.removePDFFromViewPort}
+          addPDFToViewPort={this.addPDFToViewPort}
+          removeExplorer={this.removeExplorer}
+          key={`explorer-${Date.now()}`}
+          id={Date.now()}
+          addExplorer={this.addExplorer}
+        />,
+      ];
+      return { explorers: _explorers };
+    });
+  };
 
-  removeExplorer = id => {
-    
-    this.setState( prevState => {
-      const _explorers = prevState.explorers.filter( explorer => explorer.props.id !=  id)
-      return {explorers: _explorers}
-    })
-  }
+  removeExplorer = (id) => {
+    this.setState((prevState) => {
+      const _explorers = prevState.explorers.filter(
+        (explorer) => explorer.props.id != id
+      );
+      return { explorers: _explorers };
+    });
+  };
 
-  addPDFToViewPort = filePath => {
-    this.setState( prevState => {
-      const _pdfsToView = [...prevState.pdfsToView, filePath]
-      return {pdfsToView: _pdfsToView}
-    })
-  }
+  addPDFToViewPort = (filePath, id) => {
+    this.setState((prevState) => {
+      const _pdfsToView = [...prevState.pdfsToView, { path: filePath, id: id }];
+      return { pdfsToView: _pdfsToView };
+    });
+  };
 
-  removePDFFromViewPort = filePath => {
-    this.setState(prevState => {
-      const idx = this.state.pdfsToView.indexOf(filePath)
-      const _pdfsToView = [...prevState.pdfsToView]
-      console.log(idx)
-      if (idx > -1) {
-        _pdfsToView.splice(idx,1);
-        return {pdfsToView: _pdfsToView}
-      }});
-  }
+  removePDFFromViewPort = (filePath, id) => {
+    this.setState((prevState) => {
+      return {
+        pdfsToView: [...prevState.pdfsToView].filter((pdf) => pdf.id != id),
+      };
+    });
+  };
+
+  componentDidMount = () => {
+    const newExplorers = prefs.fileExplorer.initialPaths.map((path, i) => {
+      if (i != 0) {
+        // already mounting the first explorer, regardless of prefs
+        return (
+          <FileExplorer
+            initialPath={path}
+            removeExplorer={this.removeExplorer}
+            removePDFFromViewPort={this.removePDFFromViewPort}
+            addPDFToViewPort={this.addPDFToViewPort}
+            key={"explorer-auto-" + i}
+            id={Date.now()}
+            addExplorer={this.addExplorer}
+          />
+        );
+      }
+    });
+    this.setState((prevState) => ({
+      explorers: [...prevState.explorers, ...newExplorers],
+    }));
+  };
 
   state = {
-    explorers: [<FileExplorer removeExplorer={this.removeExplorer} removePDFFromViewPort={this.removePDFFromViewPort} addPDFToViewPort={this.addPDFToViewPort} key="explorer-0" id={0} addExplorer={this.addExplorer}/>],
-    pdfsToView: []
-  }
-  
+    explorers: [
+      <FileExplorer
+        initialPath={
+          prefs.fileExplorer.initialPaths[0]
+            ? prefs.fileExplorer.initialPaths[0]
+            : null
+        }
+        removeExplorer={this.removeExplorer}
+        removePDFFromViewPort={this.removePDFFromViewPort}
+        addPDFToViewPort={this.addPDFToViewPort}
+        key="explorer-0"
+        id={0}
+        addExplorer={this.addExplorer}
+      />,
+    ],
+    pdfsToView: [],
+  };
 
   render() {
-    const listToView = this.state.pdfsToView.map ( path =>     <li key={`${path}-${Date.now()}`}>{path}</li> );
+    const listToView = this.state.pdfsToView.map((pdf) => (
+      <li key={pdf.id}>{pdf.path}</li>
+    ));
+
     return (
       <div className="mx-1">
         <Container fluid>
-          <Row style={{minHeight: "50vh"}}>
+          <Row style={{ minHeight: "50vh" }}>
             {this.state.pdfsToView.length > 0 ? <ul>{listToView}</ul> : null}
           </Row>
-          <Row style={{maxHeight: "50vh", overflow: "scroll"}}>
+          <Row style={{ maxHeight: "50vh", overflowY: "scroll" }}>
             {this.state.explorers}
           </Row>
         </Container>
--- src/FileExplorer.js
+++ src/FileExplorer.js
@@ -1,22 +1,22 @@
 import React, { useState, useEffect, Component } from "react";
 import { Col, Table } from "react-bootstrap";
-import "./App.css"
+import "./App.css";
 
 const fs = window.require("fs");
 const path = window.require("path");
 
 export default class FileExplorer extends Component {
   state = {
-    root: "/Users/chastain/pdf_toolbox/sample_pdfs",
+    root: this.props.initialPath,
     folders: [],
-    selectedRows: []
+    selectedRows: [],
   };
 
   getFolders = (directory) => {
     if (fs.existsSync(directory)) {
       return fs
         .readdirSync(directory)
-        .filter((fileName) => fileName.toUpperCase().substr(-3) === "PDF");
+        .filter((fileName) => fileName.toUpperCase().substr(-4) === ".PDF");
     } else {
       return [];
     }
@@ -30,38 +30,46 @@ export default class FileExplorer extends Component {
     this.setState({ root: e.target.value });
     this.setState({ folders: this.getFolders(e.target.value) });
   };
+
   handleAddExplorer = () => {
-    this.props.addExplorer()
-  }
+    this.props.addExplorer();
+  };
 
   handleRemoveExplorer = () => {
-    this.props.removeExplorer(this.props.id)
-  }
+    this.props.removeExplorer(this.props.id);
+  };
 
-  toggleSelected = target => {
+  toggleSelected = (target) => {
     if (target.classList.contains("selected")) {
-      target.classList.remove("selected")
+      target.classList.remove("selected");
     } else {
-      target.classList.add("selected")
+      target.classList.add("selected");
     }
-  }
+  };
 
-  handleSelectRow = e => {
-    const fullPath = path.join(this.state.root,e.target.innerText)
+  handleSelectRow = (e) => {
+    const fullPath = path.join(this.state.root, e.target.innerText);
     if (e.target.classList.contains("selected")) {
-      this.toggleSelected(e.target)
-      this.props.removePDFFromViewPort(fullPath);
+      this.toggleSelected(e.target);
+      this.props.removePDFFromViewPort(
+        fullPath,
+        `${this.props.id}-${fullPath}`
+      );
     } else {
-      this.toggleSelected(e.target)
-      this.props.addPDFToViewPort(fullPath)
+      this.toggleSelected(e.target);
+      this.props.addPDFToViewPort(fullPath, `${this.props.id}-${fullPath}`);
     }
-  
-  }
+  };
 
   render() {
     const folderList = this.state.folders.map((folder, i) => (
       <tr key={`rw-${folder}-${i}`}>
-        <td onClick={this.handleSelectRow} colSpan={2} style={{ fontSize: "9pt" }} key={`cl-${folder}-${i}`}>
+        <td
+          onClick={this.handleSelectRow}
+          colSpan={2}
+          style={{ fontSize: "9pt" }}
+          key={`cl-${folder}-${i}`}
+        >
           {folder}
         </td>
       </tr>
@@ -69,26 +77,27 @@ export default class FileExplorer extends Component {
 
     return (
       <Col className="mx-1 border border-success p-1">
-        
-        
-          <Table size="sm" striped hover>
-            <tbody>
-              <tr>
-              <td><input
-          style={{width: "100%"}}
-          placeholder="Path"
-          onChange={this.handleRootChange}
-          value={this.state.root}
-        ></input></td>
-        <td style={{textAlign: "right"}}>
-          <button onClick={this.handleAddExplorer}>+</button>
-          {this.props.id != 0 ? (<button onClick={this.handleRemoveExplorer}>-</button>) : null}
-          </td>
-              </tr>
-              {folderList}
-              </tbody>
-          </Table>
-        
+        <Table size="sm" striped hover>
+          <tbody>
+            <tr>
+              <td>
+                <input
+                  style={{ width: "100%" }}
+                  placeholder="Path"
+                  onChange={this.handleRootChange}
+                  value={this.state.root}
+                ></input>
+              </td>
+              <td style={{ textAlign: "right" }}>
+                <button onClick={this.handleAddExplorer}>+</button>
+                {this.props.id != 0 ? (
+                  <button onClick={this.handleRemoveExplorer}>-</button>
+                ) : null}
+              </td>
+            </tr>
+            {folderList}
+          </tbody>
+        </Table>
       </Col>
     );
   }
