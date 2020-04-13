db5c4f0bb637a241bf7a3f6b7db912ca807dd26c
James Chastain <jameschastain1337@gmail.com>
2020-04-13 14:02:51 -0400

    updated gitignore

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -1,42 +1,115 @@
-import React from "react"
-import {Row, Col, Button} from "react-bootstrap"
+import React from "react";
+import { Row, Col, Button } from "react-bootstrap";
+
+const { PythonShell } = window.require("python-shell");
 
 export default class Toolbar extends React.Component {
+  handleRenameClick = (e) => {
+    const options = {
+      mode: "text",
+      pythonPath:
+        "C:\\Users\\James\\AppData\\Local\\Programs\\Python\\Python37-32\\python.exe",
+      pythonOptions: ["-u"],
+      scriptPath: "C:\\Users\\James\\pdf_toolbox\\src\\scripts",
+      args: [e.target.innerText],
+    };
 
-    handleRenameClick = e => {
-        console.log(e.target.innerText)
-    }
+    PythonShell.run("main.py", options, (err, data) => {
+      console.log("Callback firing");
+      if (err) {
+        console.log(err);
+      } else {
+        window.alert(data);
+      }
+    });
+  };
 
-    handleSplitClick = e => {
-        console.log(e.target.innerText)
-    }
+  handleSplitClick = (e) => {
+    console.log(e.target.innerText);
+  };
 
-    handleMergeClick = e => {
-        console.log(e.target.innerText)
-    }
-    
-    handleArchiveClick = e => {
-        console.log(e.target.innerText)
-    }
+  handleMergeClick = (e) => {
+    console.log(e.target.innerText);
+  };
 
-    render()
-    {
-    return (    <Row className="text-center" >
-            <Col style={{margin: "auto", backgroundColor: (this.props.numberSelected === 0 ? "lightgray" : "rgb(181, 243, 179)")}}>
-                {`${this.props.numberSelected} selected`}
-            </Col>
-            <Col>
-                <Button onClick={this.handleRenameClick} variant="secondary" block size="sm" disabled={this.props.numberSelected===1 ? false : true}>Rename</Button>
-            </Col>
-            <Col>
-                <Button onClick={this.handleSplitClick} variant="secondary" block size="sm" disabled={this.props.numberSelected>1 ? false : true}>Split</Button>
-            </Col>
-            <Col>
-                <Button onClick={this.handleMergeClick} variant="secondary" block size="sm" disabled={this.props.numberSelected>1 ? false : true}>Merge</Button>
-            </Col>
-            <Col>
-                <Button onClick={this.handleArchiveClick} variant="secondary" block size="sm" disabled={this.props.numberSelected>0 ? false : true}>Archive</Button>
-            </Col>
-        </Row>)
-    }
+  handleArchiveClick = (e) => {
+    console.log(e.target.innerText);
+  };
+
+  handleParseClick = (e) => {
+    console.log(e.target.innerText);
+  };
+
+  render() {
+    return (
+      <Row className="text-center">
+        <Col
+          style={{
+            margin: "auto",
+            backgroundColor:
+              this.props.numberSelected === 0
+                ? "lightgray"
+                : "rgb(181, 243, 179)",
+          }}
+        >
+          {`${this.props.numberSelected} selected`}
+        </Col>
+        <Col>
+          <Button
+            onClick={this.handleRenameClick}
+            variant="secondary"
+            block
+            size="sm"
+            disabled={this.props.numberSelected === 1 ? false : true}
+          >
+            Rename
+          </Button>
+        </Col>
+        <Col>
+          <Button
+            onClick={this.handleSplitClick}
+            variant="secondary"
+            block
+            size="sm"
+            disabled={this.props.numberSelected > 1 ? false : true}
+          >
+            Split
+          </Button>
+        </Col>
+        <Col>
+          <Button
+            onClick={this.handleMergeClick}
+            variant="secondary"
+            block
+            size="sm"
+            disabled={this.props.numberSelected > 1 ? false : true}
+          >
+            Merge
+          </Button>
+        </Col>
+        <Col>
+          <Button
+            onClick={this.handleArchiveClick}
+            variant="secondary"
+            block
+            size="sm"
+            disabled={this.props.numberSelected > 0 ? false : true}
+          >
+            Archive
+          </Button>
+        </Col>
+        <Col>
+          <Button
+            onClick={this.handleParseClick}
+            variant="secondary"
+            block
+            size="sm"
+            disabled={this.props.numberSelected === 1 ? false : true}
+          >
+            Parse
+          </Button>
+        </Col>
+      </Row>
+    );
+  }
 }
---
+++ src/scripts/main.py
@@ -0,0 +1,8 @@
+import sys
+
+
+def printSomething(something):
+    print("You printed: " + something)
+
+
+printSomething(sys.argv[1])
